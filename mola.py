897e8aa1822d0a8390679ee4be15caab947673ce
Hugo Santana <hugosc@id.uff.br>
2017-05-22 15:20:12 -0300

    Com atrito

--- mola.py
+++ mola.py
@@ -12,11 +12,13 @@ a=[0]
 dt=0.01
 k=1
 m=1
+g=9.8
+mi=0.003
 
 while (t[-1]<=100):
 	t.append(t[-1]+dt)
 	x.append(x[-1]+v[-1]*dt+0.5*a[-1]*dt**2)
-	a.append(-(k/m)*x[-1])
+	a.append(-(k/m)*x[-1]+(-(np.sign(v[-1])*mi*g)))
 	v.append(v[-1]+0.5*(a[-2]+a[-1])*dt)
 
 plt.figure(figsize=(8,5),dpi=100)
@@ -32,12 +34,12 @@ ax.autoscale()
 
 plt.rc('text',usetex=True)
 plt.rc('font',**{'sans-serif':'Arial','family':'sans-serif'})
-plt.xlabel(r'\textnormal{Tempo} (s)')
-plt.ylabel(r'\textnormal{Posicao} (m)')
-plt.title(r'\textnormal{Oscilador sem atrito}',fontsize=12)
+plt.xlabel(r'\textnormal{Posicao} (m)')
+plt.ylabel(r'\textnormal{Velocidade} (m/s)')
+plt.title(r'\textnormal{Oscilador com atrito (velocidade $\times$ posicao)}',fontsize=12)
 
-plt.plot(t,x,'-b',linewidth=1)
+plt.plot(x,v,'-b',linewidth=1)
 plt.legend(loc="upper right")
 plt.grid()
-plt.savefig("saxt.pdf",dpi=96)
+plt.savefig("cavx.pdf",dpi=96)
 plt.show()
